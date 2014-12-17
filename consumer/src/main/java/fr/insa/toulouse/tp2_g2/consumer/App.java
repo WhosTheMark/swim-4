e4f7be8fafe86dba6507ca78b267c3d30e93e1b8
Marcos Campos <whosthemark@gmail.com>
2014-12-17 16:09:16 +0100

    Added project structure.

---
+++ consumer/src/main/java/fr/insa/toulouse/tp2_g2/consumer/App.java
@@ -0,0 +1,13 @@
+package fr.insa.toulouse.tp2_g2.consumer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
