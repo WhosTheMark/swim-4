a914017a3086fa1588f199f50ed9970bb8566580
TomVuillemin <tom.vuillemin@gmail.com>
2014-12-17 20:53:59 +0100

    Finish creating and testing Producer webService

--- Producer/src/test/java/com/swim/producer/ProducerServiceTest.java
+++ Producer/src/test/java/com/swim/producer/ProducerServiceTest.java
@@ -1,6 +1,5 @@
 package com.swim.producer;
 
-import java.nio.charset.Charset;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -13,29 +12,21 @@ public class ProducerServiceTest {
     public ProducerServiceTest() {
     }
 
+    private ProducerService instance  = new ProducerService();
     /**
      * Test of getRequest method, of class ProducerService.
      */
     @Test
-    public void testGetRequest() throws Exception {
+    public void testMethodGetRequest() throws Exception {
         System.out.println("getRequest");
-        //EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        //ProducerService instance = (ProducerService)container.getContext().lookup("java:global/classes/ProducerService");
         int expResult = 2097152; //2 Mo
-        ProducerService instance = new ProducerService(expResult, 2);
+        int waitTime = 3;
+        instance.setResponseSize(expResult);
+        instance.setProcessingTime(waitTime);
         String result = instance.getRequest();
         byte[] resultByte = instance.encodeUTF8(result);
         int size = resultByte.length;
         assertEquals(expResult, size);
-        
-        expResult = 0; 
-        instance = new ProducerService(expResult, 2);
-        result = instance.getRequest();
-        resultByte = instance.encodeUTF8(result);
-        //byte[] resultByte = result.getBytes();
-        size = resultByte.length;
-        assertEquals(expResult, size);
-        //container.close();
     }
-
+    
 }
