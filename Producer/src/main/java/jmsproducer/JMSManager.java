c006cbdf722dfcbc32bbd0b41fd42ad12b7b0ffe
Nicolas Fayolle <fayolle.n2@gmail.com>
2015-01-09 15:38:31 +0100

    JMS connection corrected + try catch to remplace throws exception + tested

---
+++ Producer/src/main/java/jmsproducer/JMSManager.java
@@ -0,0 +1,64 @@
+
+
+/**
+ * @author: N. Fayolle
+ * @date: 17/12/2014
+ * @lastRevisionDate: 17/12/2014
+ * @content: this class represents the main or the communicator in the JavaApp
+ * @comment: It will be changed but will be helpful
+ */
+package jmsproducer;
+
+import java.io.IOException;
+
+public class JMSManager {
+	protected static JMSManager jmsConnection=null;
+	private TopicAssociation topicAssociation;	
+	private ProducerReceiverThread receiver;//donner l acces
+	private String ipAdress="localhost";
+	
+	/**
+	 * Constructor
+	 * @throws IOException
+	 */
+	protected JMSManager() {
+		
+		try {
+			this.topicAssociation=new TopicAssociation(this.ipAdress);		
+			this.receiver=new ProducerReceiverThread(this.topicAssociation);
+			this.receiver.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+		
+	
+	/**
+	 * Singleton de JMS Connection
+	 * @return
+	 * @throws IOException 
+	 */
+	public synchronized static JMSManager getInstance() throws IOException{
+		
+		if(jmsConnection==null){
+			jmsConnection=new JMSManager();
+		}
+		return jmsConnection;
+	}
+
+
+
+	public ProducerReceiverThread getReceiver() {
+		return receiver;
+	}
+
+
+	
+	
+
+
+
+	
+	
+}
---
+++ consumer/src/main/java/jmsconsumer/ConsumerSender.java
@@ -0,0 +1,32 @@
+/**
+ * @author: N. Fayolle
+ * @date: 17/12/2014
+ * @lastRevisionDate: 17/12/2014
+ * @content: this class is in charge of sending a message to the queue
+ * @comment: 
+ */
+
+package jmsconsumer;
+
+import java.io.IOException;
+
+import javax.xml.ws.handler.MessageContext;
+
+import jmsmainapp.QueueAssociation;
+
+import messaging.Message;
+
+public class ConsumerSender {
+	private QueueAssociation queueAssociation;
+	
+	public ConsumerSender(QueueAssociation queueAssociation){
+		this.queueAssociation=queueAssociation;
+	}
+	
+	public synchronized void send(Message message) throws IOException{
+		this.queueAssociation.getChannel().basicPublish("", this.queueAssociation.getQUEUE_NAME(), null, message.toJson().getBytes());
+		System.out.println(" [x] Sent '" + message + "'");
+		this.queueAssociation.getChannel().close();
+		this.queueAssociation.getConnection().close();
+	}
+}
