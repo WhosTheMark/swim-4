45c193de5abfd8019418dded38f78877780ac85b
TomVuillemin <tom.vuillemin@gmail.com>
2015-01-15 13:32:40 +0100

    merge all of the Producer project

---
+++ mainApp/src/main/java/controller/SWIM.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class SWIM {
+
+	public static void main(String[] args) {
+		if(args.length != 1) {
+			System.out.println("ERROR - Bad arguments");
+			System.out.println("you need to provide the name of the xml file you want to use ase scenario");
+		} else {
+			
+		}
+	}
+}
