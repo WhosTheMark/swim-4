57d35d8ac8539814fd0abd9368f8d5267b6417ec
marjorieDugrand <marjorie.dugrand@gmail.com>
2015-01-19 09:52:33 +0100

    message factory first draft

---
+++ mainApp/src/test/java/fr/insa/toulouse/tp2g2/mainApp/MessageRetrievedFromJsonTest.java
@@ -0,0 +1,14 @@
+package fr.insa.toulouse.tp2g2.mainApp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MessageRetrievedFromJsonTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
