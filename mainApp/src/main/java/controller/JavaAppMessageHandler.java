1a8ea8930e91a28c9519ef36563b733ac8cc93fc
Marcos Campos <whosthemark@gmail.com>
2015-01-20 10:42:12 +0100

    Added method to handle OK messages from consumers. Also modified the List of messages to a BlockingQeue of messages.

--- mainApp/src/main/java/controller/JavaAppMessageHandler.java
+++ mainApp/src/main/java/controller/JavaAppMessageHandler.java
@@ -2,7 +2,9 @@ package controller;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.BlockingQueue;
 
+import messaging.Message;
 import messaging.MessageError;
 import messaging.MessageFactory;
 import messaging.MessageResult;
@@ -11,29 +13,33 @@ import messaging.MessageType;
 public class JavaAppMessageHandler extends Thread {
 
 	private MessageFactory factory;
-	private List<String> messages;
+	private BlockingQueue<String> messages;
 	private SWIMController controller;
+	private List<String> consumers;
 	
-	public JavaAppMessageHandler(List<String> messages, SWIMController controller) {
+	public JavaAppMessageHandler(BlockingQueue<String> messages, SWIMController controller,
+	                             List<String> consumers) {
 		factory = MessageFactory.getInstance();
 		this.messages = messages;
 		this.controller = controller;
+		this.consumers = consumers;
 	}
 	
 	public void run() {
 		while(controller.keepRunning()) {
-			Iterator<String> it = messages.iterator();
-			while(it.hasNext()) {
-				handleMessage(it.next());
-				it.remove();
-			}
+			try {
+                String json = messages.take();
+                handleMessage(json);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 		}
 	}
 	
 	private void handleMessage(String json) {
 		MessageType type = factory.identifyMessage(json);
 		switch(type) {
-		case RESULT: 
+		case RESULT:
 			MessageResult result = factory.getMessageResultFromJson(json);
 			result.store();
 			break;
@@ -42,15 +48,22 @@ public class JavaAppMessageHandler extends Thread {
 			error.store();
 			break;
 		case OK:
-			handleTerminatedConsumerMessage();
+			handleTerminatedConsumerMessage(json);
 	    default:
 	    	throw new SWIMException("ERROR - message of type " + type + " not expected" );
 		}
 		
 	}
 	
-	private void handleTerminatedConsumerMessage() {
+	private void handleTerminatedConsumerMessage(String json) {
 		
+	    Message msg = factory.getMessageFromJson(json);
+		String consumerName = msg.getFrom();
+		consumers.remove(consumerName);
+		
+		if(consumers.isEmpty()){
+		    controller.handleAllConsumersHaveFinished();
+		}
 	}
 	
 	
