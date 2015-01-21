075218667d3175a031bc252a14ec7127ecdc622d
Stephane Sallaberry <pikachat@outlook.com>
2015-01-21 12:23:21 +0100

    Database.dropDatabase()

--- mainApp/src/main/java/database/Database.java
+++ mainApp/src/main/java/database/Database.java
@@ -1,5 +1,24 @@
 package database;
 
+import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.node.Node;
+
 public class Database {
 	public final static String DATABASE_NAME = "swim";
+	
+	public final static void dropDatabase(){
+		
+		Node node = nodeBuilder().client(true).node();
+		Client client = node.client();
+		
+		client.prepareDeleteByQuery(DATABASE_NAME)
+        	  .setQuery(QueryBuilders.matchAllQuery())
+        	  .execute().actionGet();
+		
+		client.close();
+		node.close();
+	}
 }
