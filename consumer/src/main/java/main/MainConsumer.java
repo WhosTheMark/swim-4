ae7640b9335ee50a5665989dcf23112bf61dd2be
Marcos Campos <whosthemark@gmail.com>
2015-01-21 15:11:09 +0100

    Fixed infinite loop in consumer.

--- consumer/src/main/java/main/MainConsumer.java
+++ consumer/src/main/java/main/MainConsumer.java
@@ -7,8 +7,9 @@ import jmsconsumer.JMSManager;
 
 public class MainConsumer {
 	
+    // TODO Set name of consumer and esb address
 	private static String consumerName = "consumer" ;
-	private static String esbAddr = null ; // à remplir
+	private static String esbAddr = null ;
 	
 	public static void main(String[] args) throws IOException, InterruptedException {
 		JMSManager manager = JMSManager.getInstance();
